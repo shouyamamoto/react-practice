112e576f58e526f78e02b8b9fafbe8a3129236d6
shou yamamoto <yamasyo0017@gmail.com>
2021-03-01 11:43:07 +0900

    add onClickAdd

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,13 +3,24 @@ import './App.css'
 import { useEffect, useState } from 'react'
 
 const App = () => {
+  const [todoText, setTodoText] = useState('')
   const [incompleteTodos, setIncompleteTodos ] = useState(['あああ', 'いいいい'])
   const [completeTodos, setcompleteTodos ] = useState(['うううう'])
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value)
+
+  const onClickAdd = () => {
+    if(todoText === '') return
+    const newTodos = [...incompleteTodos, todoText]
+    setIncompleteTodos(newTodos)
+    setTodoText('')
+  }
+
   return(
     <>
       <div className="input-area">
-        <input type="text" placeholder="TODOを入力" />
-        <button>追加</button>
+        <input type="text" placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}/>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
